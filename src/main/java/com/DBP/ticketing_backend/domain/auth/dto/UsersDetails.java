b4e9142a936350d891e448a4fedc2f9a8eaedcdf
Go Yeongmin <ko07073@gmail.com>
2025-11-14 00:27:31 +0900

    refactor: Userdetails에 userId, name, phoneNumber 추가

--- src/main/java/com/DBP/ticketing_backend/domain/auth/dto/UsersDetails.java
+++ src/main/java/com/DBP/ticketing_backend/domain/auth/dto/UsersDetails.java
@@ -2,6 +2,7 @@ package com.DBP.ticketing_backend.domain.auth.dto;
 
 import com.DBP.ticketing_backend.domain.users.entity.Users;
 
+import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -11,14 +12,26 @@ import java.util.List;
 
 public class UsersDetails implements UserDetails {
 
+    @Getter
+    private final Long userId;
+
     private final String email;
     private final String password;
     private final String role;
 
+    @Getter
+    private final String name;
+
+    @Getter
+    private final String phoneNumber;
+
     public UsersDetails(Users users) {
+        this.userId = users.getUserId();
         this.email = users.getEmail();
         this.password = users.getPassword();
         this.role = users.getRole().getKey();
+        this.name = users.getName();
+        this.phoneNumber = users.getPhoneNumber();
     }
 
     @Override
