b188c4f8adb86e04cd70cc07f514fec045e32092
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2025-11-17 19:12:55 +0900

    feat: rename user roles to include ROLE prefix

--- src/main/java/com/DBP/ticketing_backend/domain/users/enums/UsersRole.java
+++ src/main/java/com/DBP/ticketing_backend/domain/users/enums/UsersRole.java
@@ -6,9 +6,9 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum UsersRole {
-    USER("USER", "일반 사용자"),
-    ADMIN("ADMIN", "관리자"),
-    HOST("HOST", "호스트");
+    ROLE_USER("ROLE_USER", "일반 사용자"),
+    ROLE_ADMIN("ROLE_ADMIN", "관리자"),
+    ROLE_HOST("ROLE_HOST", "호스트");
 
     private final String key;
     private final String description;
